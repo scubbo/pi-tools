9d109da01f21b6cb84a0ed7bc9c8c753a46b664f
Jack Jackson <scubbojj@gmail.com>
2021-08-27 11:00:42 -0700

    Move to Python for scp-ing backups
    Still haven't implemented Prometheus metrics for this yet.

--- hass-backup-sync.sh
+++
@@ -1,6 +0,0 @@
-cd /mnt/BERTHA/ha_backups
-rm *.tar
-scp -r -o StrictHostKeyChecking=no hass:/backup/*.tar .
-
-# I wish there was a good scp module for Python but paramiko succccksssss
-python3 -c "import os, tarfile json, datetime; transformed = [json.load(tarfile.open(name).extractfile('./snapshot.json')) for name in os.listdir('.') if name.endswith('.tar')]; latest = sorted(transformed, key=lambda snap: datetime.datetime.strptime(snap['date'], '%Y-%m-%dT%H:%M:%S.%f%z'))[-1]; metadata_file = open('metadata', 'w'); metadata_file.write('latest_file:' + latest['slug'] + '.tar\ntimestamp:' + latest['date'] + '\n'); metadata_file.close()"
