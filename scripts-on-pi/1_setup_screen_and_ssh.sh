da161aa46654f947ade27c3c5151d7514cd4fba1
Jack Jackson <scubbojj@gmail.com>
2022-05-17 20:51:53 -0700

    Correction to authorized-key setup
    Turns out the mask is a _negation_ :D
    
    Also uses a neat trick from
    [here](https://sneak.berlin/20191011/stupid-unix-tricks/) to pull keys
    from GitHub.

--- scripts-on-pi/1_setup_screen_and_ssh.sh
+++ scripts-on-pi/1_setup_screen_and_ssh.sh
@@ -19,13 +19,13 @@ apt-get -y upgrade
 apt-get install -y screen
 
 ###
-# Install ssh key...
+# Install ssh keys...
 ###
-(umask 700 && test -d /home/pi/.ssh || mkdir /home/pi/.ssh)
-(umask 644 && touch /home/pi/.ssh/authorized_keys)
+(umask 077 && test -d /home/pi/.ssh || mkdir /home/pi/.ssh)
+(umask 177 && touch /home/pi/.ssh/authorized_keys)
 chown pi /home/pi/.ssh/authorized_keys
 chgrp pi /home/pi/.ssh/authorized_keys
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCyQIZAWHbGpiSQHaD8ph33Q5PWJy72OyqzcBCVmyUnXcaP1YpebSxbHUhQhKDv1i1OBnsxIO1NDo60h0/2ygCHYNpSVjgS2P+37jBDaNIypB+kJ0emaKGjkZF5Nlc45U1wOcpGTze+ysCLLP/ijPXjQioH2RkCwTvSgKfKcmMqSmxA76JarL66Gva3mlOSRBoRVq0YDxUrIELlVjZ602UTzeVjhx6A8XecvkZEI4YlJ38lNSJd3fegceWd/sH1NYWKWZyq5HHe74EG9o/icuCvddG6V+7qpYGGizXemYOdaS94OPZ8J577nHKBFtQJxA8a34UG5CkNdWePaIco7b2b8tx+yE78yWIOFRL3JtR+jZzZP9Jz+0ChmhMsk/2dMDtd0u32ZpMaj2t/82kP4omslUij/oU1pUIvna03WagCcUpjNnSU05a0mLcRg+EhwFKe+xXaniY9fiGiA2pknwxWURRUfnOwBPivpYh1rAG0Jh19R4fpJUsW16hG9IAdWPrZsvDpRaaIhh1y6T76tHrAux8xExhxydoLKl8bzNSGcu0QTvmBoRlBFLJj/3x9VyIunatHb0KYnvDs+9YL6LSEXYdWqwV0uZyk6fg9G/eIqB+i4ZhnbKbHR/cCy3MkktPIILpE75lQdmgHsOaSdwywb1PRzYUNFVNiRzW6IvH+XQ== scubbojj@gmail.com" >> /home/pi/.ssh/authorized_keys
+curl --silent https://github.com/scubbo.keys >> /home/pi/.ssh/authorized_keys
 echo "Finished updating ssh authorized_keys"
 
 ###
