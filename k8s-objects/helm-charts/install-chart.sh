e60dc14b7b71b2749423b0871a00e01604af686d
Jack Jackson <scubbojj@gmail.com>
2022-10-30 14:00:46 -0700

    Enable installation of a single Helm chart

--- k8s-objects/helm-charts/install-chart.sh
+++ k8s-objects/helm-charts/install-chart.sh
@@ -60,6 +60,40 @@ if [[ -d "out" ]]; then
 fi
 mkdir -p out/
 
-find . -type d -depth 1 -not -name 'out' -not -name 'out-old' -exec bash -c 'set -uo pipefail; addRepo "$@"' bash {} \;
+while getopts n:a flag
+do
+  case "${flag}" in
+    n) name=${OPTARG};;
+    a) all=1;;
+    *) echo "Unknown flag detected"; exit 1;;
+  esac
+done
+
+if [ -n "${name+x}" ] && [ -n "${all+x}" ]; then
+  echo "You cannot set both -n and -a. Try again";
+  exit 1
+fi
+
+if [ -z ${name+x} ] && [ -z ${all+x} ]; then
+  echo "You must set one of -n [name] or -a.";
+  exit 1
+fi
+
+if [ -n "${name+x}" ]; then
+  names="./$name"
+fi
+if [ -n "${all+x}" ]; then
+  names=$(find . -type d -depth 1 -not -name 'out' -not -name 'out-old' | tr '\n' ':')
+fi
+
+IFS=':' read -ra ARR <<< "$names"
+for dir in "${ARR[@]}"; do
+  addRepo "$dir"
+done
+
 helm repo update
-find . -type d -depth 1 -not -name 'out' -not -name 'out-old' -exec bash -c 'set -uo pipefail; process "$@"' bash {} \;
+
+IFS=':' read -ra ARR <<< "$names"
+for dir in "${ARR[@]}"; do
+  process "$dir"
+done
